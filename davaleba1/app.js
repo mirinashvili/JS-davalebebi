a0128bbde09c6e6f3f661592d7d9ba3725373a7d
mamuka <mamuka.mirinahvili.1@iliauni.edu.ge>
2022-03-04 17:58:26 +0400

    work

---
+++ davaleba1/app.js
@@ -0,0 +1,72 @@
+// დავალება-1
+
+// let user ={
+    // firstname:'giorgi',
+    // lastname:'smith',
+    // age:25,
+    // studentstatus:'active',
+// } 
+
+// console.log(user.studentstatus);  
+
+// დავალება-2
+
+//  let numbers = [2, 6, 10, 23, 3, 7, 4, 50, 28, 26, 79];
+// for(let i=0; i<numbers.length;i++)
+// console.log(numbers[i])
+// let item = 0;
+// while (item<numbers.length){
+    // console.log(numbers[item]);
+    // item++
+// }
+
+// დავალება-3
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for (let i = 0; i < numbers.length; i++) {
+    // if (numbers[i] > 5) {
+        // console.log(numbers[i]);
+    // }
+    
+// }
+
+// დავალება-4
+
+//  let users =  [
+    // {username:'test1', status: false},
+    // {username:'test2', status: false},
+    // {username:'test3', status: true}
+//  ];
+//  console.log(users[2])
+
+// დავალება-5
+
+// let user = {
+	// name: 'giorgi',
+	// age:  20,
+	// studentstatus: 'active'
+// }
+
+// if(user.age > 18 && user.studentstatus == 'active'){
+    //    console.log("hello")}
+
+// if(user.name=='giorgi'){
+    // console.log('hello giorgi')
+// }
+
+//  if(user.studentstatus=='active' || user.age < 25){
+    //  console.log('hello world')
+// } else {
+    // console.log('error')
+// }
+
+// დავალება -6
+
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+// for ( let i of array){
+    // if(typeof i == 'string'){
+    // console.log(i);
+    // }
+// }
+
