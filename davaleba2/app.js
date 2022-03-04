eca892dd3d5d5e4f07a84e048770a0c6574e3241
mamuka <mamuka.mirinahvili.1@iliauni.edu.ge>
2022-03-04 18:00:26 +0400

    meore

---
+++ davaleba2/app.js
@@ -0,0 +1,69 @@
+// დავალება-1
+
+// let result = numbers(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8)
+
+// function numbers (...n){
+    // let Numbers = 0;
+
+//    for (let x of n) {
+    //    if (x > 0) {
+        //    Numbers = Numbers + x;
+    //    }
+//    }
+//    console.log(Numbers);
+// }
+
+// დავალება-2
+
+// Ns (10, 50, 6, 7, 8, 11, 6, 3, 9)
+
+// function Ns (...array) {
+    // let cont = 0;
+
+    // for (let x of array) {
+        // cont = cont + x;
+    // }
+//    console.log(cont);
+// }
+
+// დავალება-3
+
+// let user = {
+    // firstname: 'giorgi',
+    // lastname: 'saakadze',
+    // age: 32,
+    // isloggedin: true
+//   }
+
+// function test (obj) {
+    // if (obj.isloggedin == true) {
+        // return obj.firstname + '' + obj.lastname;
+    // }
+    // return false;
+// }
+// let result = test(user);
+// console.log(result);
+
+// დავალება-4
+
+// function maxNumber (value){
+    // let max = 0;
+
+    // for (let i=0; i < value.length; i++) {
+        // const x = value [i];
+
+        // if (x > max) {
+            // max = x;
+        // }
+
+    // }
+
+    // return max;
+
+// }
+
+// console.log(maxNumber([3,5,22,11,54,23]));
+
+
+
+
